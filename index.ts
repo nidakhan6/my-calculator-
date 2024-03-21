6bc2e6823fe0a0a1ef9c7fb73afe8db69f41fcf5
NidaWorld <160415456+nidakhan6@users.noreply.github.com>
2024-03-22 00:45:45 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,6 +1,40 @@
+#!/usr/bin/env node
 
-let greet = "Hello";
+// my first project is simple calculator
 
-let userName = 'Nidakhan';
 
-console.log(greet + '' + userName);
+// printing a Welcome Message
+// console.log ("\n\tWelcome To \ 'codewithnida\' - CLI simple calculator\n");
+
+// Asking Question from users through inquirer
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await  inquirer.prompt([
+{ message: chalk.red.bold("Enter first number"), type: "number", name: "firstNumber" },
+{ message: chalk.red.bold("Enter second number"), type: "number", name: "secondNumber" },
+{
+message: chalk.blue("Select one of the operators to perform operation"),
+type: "list",
+name: "operator",
+choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+},
+]);
+
+//console.log(answer)
+
+//conditional statement
+
+if (answer.operator === "Addition") {
+console.log(chalk.green.bold(answer.firstNumber + answer.secondNumber));
+} else if (answer.operator === "Subtraction") {
+console.log(chalk.green.bold(answer.firstNumber - answer.secondNumber));
+} else if (answer.operator === "Multiplication") {
+console.log(chalk.green.bold(answer.firstNumber * answer.secondNumber));
+} else if (answer.operator === "Division") {
+console.log(chalk.green.bold(answer.firstNumber / answer.secondNumber));
+} else {
+console.log("Please select a valid operator");
+}
+
